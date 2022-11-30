1910a762b3842ac5bd6d2a85fc1dd6e38b6017c2
Alberto <albertoflj@gmail.com>
2022-11-30 17:02:27 +0200

    Added categories, not working and needs work.

--- src/compontents/Main Page Components/SlideShow/Slides.jsx
+++ src/compontents/Main Page Components/SlideShow/Slides.jsx
@@ -1,39 +1,36 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
+import "./Slides.scss";
+
 const Slides = (props) => {
-  console.log(props.produts);
   const [products, setProducts] = useState(props.products);
 
-  console.log();
   return (
-    <div className="content-right-side">
+    <div className="content-right-side desktop">
       <div className="slide-show">
         <img
           src={products[0].image.url}
           alt="image1"
-          className="slide-show-product "
+          className="slide-show-product"
         />
         <img
           src={products[1].image.url}
           alt="image2"
-          className="slide-show-product dnon"
+          className="slide-show-product"
         />
         <img
           src={products[2].image.url}
           alt="image3"
-          className="slide-show-product dnon"
+          className="slide-show-product"
         />
         <img
           src={products[3].image.url}
           alt="image4"
-          className="slide-show-product dnon"
+          className="slide-show-product"
         />
       </div>
-      <button className="slideshow-button image-1"></button>
-      <button className="slideshow-button image-2"></button>
-      <button className="slideshow-button image-3"></button>
-      <button className="slideshow-button image-4"></button>
+      <button className="shop-now shadow mobile-medium">Shop Now</button>
     </div>
   );
 };
--- src/compontents/Pages/MainPage/MainPage.jsx
+++ src/compontents/Pages/MainPage/MainPage.jsx
@@ -1,25 +1,58 @@
 import Header from "../../Header/Header";
 import "./MainPage.scss";
 import Slides from "../../Main Page Components/SlideShow/Slides";
+import MenCategoryImage from "../../../Assets/Products/men/photo_1a.jpg";
+import WomenCategoryImage from "../../../Assets/Products/women/1.jpg";
+import { Link } from "react-router-dom";
+
 const MainPage = (props) => {
   return (
     <>
       <Header />
       <div className="main-page-content padding">
-        <div className="content-left-side">
-          <div className="main-text">
-            <h1 className="page-title">
-              Lorem ipsum dolor sit amet consectetur.
-            </h1>
-            <p className="page-description">
-              Lorem ipsum dolor sit amet consectetur. Ornare consectetur in
-              consectetur magna. Nec integer nisi in fusce ornare ultrices porta
-              faucibus id.
-            </p>
+        <section className="hero-section">
+          <div className="content-left-side">
+            <div className="main-text">
+              <h1 className="page-title">
+                Lorem ipsum dolor sit amet consectetur.
+              </h1>
+              <p className="page-description">
+                Lorem ipsum dolor sit amet consectetur. Ornare consectetur in
+                consectetur magna. Nec integer nisi in fusce ornare ultrices
+                porta faucibus id.
+              </p>
+            </div>
+            <button className="shop-now shadow desktop-medium">Shop Now</button>
           </div>
-          <button className="shop-now shadow">Shop Now</button>
-        </div>
-        <Slides products={props.products} />
+          <Slides products={props.products} />
+        </section>
+
+        <section className="categories-section">
+          <div className="categories-men cat-centr">
+            <Link to="/men" className="cat-man">
+              <div className="category-image">
+                {/* <img src={MenCategoryImage} alt="men-category" /> */}
+              </div>
+            </Link>
+            <div className="category-text">
+              <Link to="/men">
+                <h2>Men's Collection</h2>
+              </Link>
+            </div>
+          </div>
+          <div className="categories-women cat-centr">
+            <Link to="/women" className="cat-woman">
+              <div className="category-image">
+                {/* <img src={WomenCategoryImage} alt="women-cateogry" /> */}
+              </div>
+            </Link>
+            <div className="category-text">
+              <Link to="/women">
+                <h2>Women's Collection</h2>
+              </Link>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );

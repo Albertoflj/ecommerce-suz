9218e2b307a02eeed1dfe98d72000d227c3e4726
Alberto <albertoflj@gmail.com>
2023-01-15 19:17:04 +0200

    Added links

--- src/compontents/Footer/Footer.jsx
+++ src/compontents/Footer/Footer.jsx
@@ -5,27 +5,23 @@ import linkedinIcon from "../../Assets/icons/icons/linkedin.svg";
 
 import { Link } from "react-router-dom";
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="footer-container">
-                
-                    <Link to="/about" className="underline-animation">
-                        <h3>About</h3>
-                    </Link>
-                    <div className="footer-sm">
-                        <a href="https://www.github.com">
-                        <img src={githubIcon} alt="github"></img>
-                        </a>
-                        <a href="https://www.linkedin.com">
-                            
-                        <img src={linkedinIcon} alt="linkedin"></img>
-                        </a>
-                   </div>
-                </div>
-          
-
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <Link to="/about" className="underline-animation">
+          <h3>About</h3>
+        </Link>
+        <div className="footer-sm">
+          <a href="https://github.com/Albertoflj">
+            <img src={githubIcon} alt="github"></img>
+          </a>
+          <a href="https://www.linkedin.com/in/abagiualberto/">
+            <img src={linkedinIcon} alt="linkedin"></img>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
